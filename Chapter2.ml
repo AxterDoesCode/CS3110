820134349ef8be7f5c0ed086b77c9447b9f5538e
AxterDoesCode <chau.alex88@hotmail.com>
2023-09-30 10:48:46 +0100

    Finished date fun

--- Chapter2.ml
+++ Chapter2.ml
@@ -32,4 +32,25 @@ let _ = assert (double 12 = 24)
 
 (** Exercise: more fun [*]
    *)
+let cube x = x *. x *. x
+let sign x = if x > 0 then 1 else if x = 0 then 0 else -1
+let circle r = Float.pi *. r ** 2.
 
+let close_enough a b = Float.abs (a -. b) < 1e-5
+
+let _ = assert (close_enough (circle 1.) Float.pi)
+
+(** Exercise: RMS [**]
+   *)
+let rms x y = ((x ** 2.) +. (y ** 2.)) /. 2. |> Float.sqrt
+let _ = assert (close_enough (rms 2. 2.) 2.)
+
+(** Exercise: date fun [***]
+   *)
+let datefun d m = if m = "Jan" || m = "Mar" || m = "May" || m = "Jul" || m = "Aug" || m = "Oct" || m = "Dec" 
+    then 1 <= d && d <= 31
+    else if m = "Apr" || m = "Jun" || m = "Sept" || m = "Nov"
+    then 1 <= d && d <= 30
+    else if m = "Feb"
+    then 1 <= d && d <= 28
+    else false
